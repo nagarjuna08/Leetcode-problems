36ece9eff9ad2f9579104fe587b09d0e2f1e58e2
Nagarjuna <nagarjunakakunuri449@gmail.com>
2025-10-25 09:43:56 +0530

    Time: 1 ms (56.85%), Space: 40.4 MB (76.33%) - LeetHub

---
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int totalMoney(int n) {
+        int monday=0;
+        int count=0;
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(count==7)
+            {
+                monday=i/7+1;
+                count=0;
+            }
+            else
+            {
+                monday++;
+            }
+            sum=sum+monday;
+            count++;
+        }
+        return sum;
+    }
+}
