4bbc00df790cf7cef62764c1463b38a65ec11384
Nagarjuna <nagarjunakakunuri449@gmail.com>
2026-02-19 09:46:53 +0530

    Time: 909 ms (5.82%), Space: 0B (100%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -0,0 +1,6 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  RETURN (
+    select  distinct e1.salary from Employee e1 where N-1=(select count(Distinct(e2.salary)) from Employee e2 where e2.salary > e1.salary)
+  );
+END
