048ea76eef48a219577afc2f7f4f229e42722d8a
Nagarjuna <nagarjunakakunuri449@gmail.com>
2026-05-31 09:18:23 +0530

    Time: 25 ms (93.3%), Space: 107.2 MB (79.91%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.java
+++ 2126-destroying-asteroids/2126-destroying-asteroids.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean asteroidsDestroyed(int mass, int[] asteroids) 
     {
-        double ma=mass;
-        Arrays.sort(asteroids);
-        for(int i=0;i<asteroids.length;i++)
-        {
-            if(ma>=asteroids[i])
-            {
-                ma+=asteroids[i];
-                System.out.println(asteroids[i]);
+         Arrays.sort(asteroids);          // Sort asteroids by size
+        long currentMass = mass;         // Use long to avoid overflow
+        
+        for (int asteroid : asteroids) {
+            if (asteroid > currentMass) {
+                return false;            // Cannot absorb this asteroid
             }
-            else
-            {
-                return false;
-            }
-        }    
+            currentMass += asteroid;     // Absorb asteroid and grow
+        }
+        
         return true;
     }
 }
