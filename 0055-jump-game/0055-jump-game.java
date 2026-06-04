ab14bcd05c23f85fc7b765837b3fb219f9aa557c
Nagarjuna <nagarjunakakunuri449@gmail.com>
2026-06-04 19:00:33 +0530

    Time: 2 ms (87.99%), Space: 48 MB (41.32%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) 
+    {
+        int res=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++)
+        {
+            if(res<i)
+            {
+                return false;
+            }
+            res=Math.max(res,i+nums[i]);
+        }   
+        return true; 
+    }
+}
