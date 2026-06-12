1ea0da00892fe0f29e9cc99f9cd0c0654704752c
Nagarjuna <nagarjunakakunuri449@gmail.com>
2026-06-12 10:04:38 +0530

    Time: 464 ms (67.79%), Space: 0B (100%) - LeetHub

--- 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
+++ 0586-customer-placing-the-largest-number-of-orders/0586-customer-placing-the-largest-number-of-orders.sql
@@ -1,2 +1,2 @@
-/* Write your PL/SQL query statement below */
-select customer_number from (select customer_number from Orders group by customer_number order by count(*)DESC ) where ROWNUM=1;
+/* Write your T-SQL query statement below */
+select top 1 customer_number from Orders group by customer_number order by count(*) desc;
