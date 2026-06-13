61c6b78a48af123ba1f1c2a35a72068cecf83be3
Nagarjuna <nagarjunakakunuri449@gmail.com>
2026-06-13 09:38:37 +0530

    Time: 93 ms (5.75%), Space: 54.5 MB (5.85%) - LeetHub

---
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int[][] merge(int[][] arr) {
+        Arrays.sort(arr, Comparator.comparingInt(a -> a[0]));
+        boolean over[]=new boolean[arr.length];
+        int n=arr.length;
+        int j;
+        for(int i=0;i<n-1;i++)
+        {
+            j=i+1;
+            while(j<n && !over[i] && arr[j][0]>=arr[i][0] && arr[j][0]<=arr[i][1])
+            {
+                int ele= Math.max(arr[i][1],arr[j][1]);
+                arr[i][1]=ele;
+                over[j]=true;
+                j++;
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(arr[i][0]+" "+ arr[i][1]);
+        }
+        int m=0;
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(over[i]);
+            if(over[i]==false)
+            {
+                m++;
+            }
+        }
+        int res[][]=new int[m][2];
+        j=0;
+        for(int k=0;k<n;k++)
+        {
+            if(!over[k])
+            {
+                res[j][0]=arr[k][0];
+                res[j][1]=arr[k][1];
+                j++;
+            }
+        }
+        return res;
+    }
+}
