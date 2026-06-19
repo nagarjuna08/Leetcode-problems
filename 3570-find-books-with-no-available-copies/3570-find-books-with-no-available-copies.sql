6765d6c9623ffd64c2d03820a73ad462b296c66f
Nagarjuna <nagarjunakakunuri449@gmail.com>
2026-06-19 09:49:13 +0530

    Time: 1284 ms (5.43%), Space: 0B (100%) - LeetHub

---
+++ 3570-find-books-with-no-available-copies/3570-find-books-with-no-available-copies.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+
+
+select l.book_id,l.title,l.author,l.genre,l.publication_year,l.total_copies as current_borrowers
+from library_books l inner join borrowing_records b on l.book_id=b.book_id 
+where b.return_date is null
+group by l.book_id,l.title,
+         l.author,
+         l.genre,
+         l.publication_year,
+         l.total_copies having count(*)=l.total_copies
+         order by current_borrowers desc,l.title ;
