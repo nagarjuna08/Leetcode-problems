8e073145f78125641a63db0d1db467d68fd535f3
Nagarjuna <nagarjunakakunuri449@gmail.com>
2026-06-19 09:36:59 +0530

    Time: 2419 ms (54.01%), Space: 0B (100%) - LeetHub

--- 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
+++ 1084-sales-analysis-iii/1084-sales-analysis-iii.sql
@@ -1,8 +1,7 @@
 /* Write your T-SQL query statement below */
 
-select product_id,product_name
-from
-    (select p.product_id,p.product_name,max(s.sale_date) as last_date,min(s.sale_date) as first_date
+
+select p.product_id,p.product_name
     from Product p join Sales s on p.product_id=s.product_id
-    group by p.product_id,product_name) res
-where ('2019-01-01'<=first_date) and (last_date<='2019-03-31')
+    group by p.product_id,product_name
+having '2019-01-01'<=min(s.sale_date) and max(s.sale_date)<='2019-03-31'
