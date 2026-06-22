897f0e0ecd16f5f8e2fef7c3993fd71e92056f72
Nagarjuna <nagarjunakakunuri449@gmail.com>
2026-06-22 10:12:11 +0530

    Time: 815 ms (6.32%), Space: 0B (100%) - LeetHub

---
+++ 3497-analyze-subscription-conversion/3497-analyze-subscription-conversion.sql
@@ -0,0 +1,16 @@
+/* Write your MySQL query statement below */
+SELECT user_id, 
+    ROUND(
+        SUM(CASE WHEN activity_type = 'free_trial' THEN activity_duration ELSE 0 END) * 1.0 /
+        COUNT(CASE WHEN activity_type = 'free_trial' THEN 1 END)
+    , 2) AS trial_avg_duration, 
+    ROUND(
+        SUM(CASE WHEN activity_type = 'paid' THEN activity_duration ELSE 0 END) * 1.0 /
+        COUNT(CASE WHEN activity_type = 'paid' THEN 1 END)
+    , 2) AS paid_avg_duration 
+FROM UserActivity 
+GROUP BY user_id 
+HAVING 
+    COUNT(CASE WHEN activity_type = 'free_trial' THEN 1 END) > 0 AND
+    COUNT(CASE WHEN activity_type = 'paid' THEN 1 END) > 0
+ORDER BY user_id;
